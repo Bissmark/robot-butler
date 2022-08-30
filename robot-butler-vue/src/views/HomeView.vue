c81247a05e1913e6dae770778df4487d58829095
Christopher Holt <holt.christopher1@gmail.com>
2022-08-30 13:42:44 +1000

    Added the embedded video

--- robot-butler-vue/src/views/HomeView.vue
+++ robot-butler-vue/src/views/HomeView.vue
@@ -1,22 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>The Robot Butler</h1>
+    <img class="robot" alt="robot" src="../assets/robot.jpeg">
+    <LazyYoutube
+      ref="lazyVideo"
+      src="https://www.youtube.com/watch?v=8LJOa0RQZBk&ab_channel=CNET"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { LazyYoutube } from "vue-lazytube";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'YourComponent',
+    components: {
+        LazyYoutube
+    },
 }
 </script>
 
 <style>
-  
+.robot {
+  max-width: 400px;
+}
 </style>
