7879282f0ada03274eb1e665d8e14cd6fe4b8c79
Lukas Eichler <lukas.eichler@yahoo.com>
2022-08-30 19:40:24 +1000

    trying to fix up input boxes

--- robot-butler-vue/src/views/HomeView.vue
+++ robot-butler-vue/src/views/HomeView.vue
@@ -15,17 +15,36 @@
     
     <form>
       <div>
-        <input type="" placeholder="Name (first)">
+        <input type="text" placeholder="Name (first)">
         <p class="input">Must contain a minimum of two letters and no numbers</p>
       </div>
       <div>
-        <input type="" placeholder="Name (last)">
+        <input type="text" placeholder="Name (last)">
         <p class="input">Must contain a minimum of two letters and no numbers</p>
       </div>
       <div>
         <input type="" placeholder="Email">
         <p class="input">Must contain an @ and at least one '.'</p>
       </div> 
+      <div role="group">
+    <label for="input-live">Name:</label>
+    <b-form-input
+      id="input-live"
+      v-model="name"
+      :state="nameState"
+      aria-describedby="input-live-help input-live-feedback"
+      placeholder="Enter your name"
+      trim
+    ></b-form-input>
+
+    <!-- This will only be shown if the preceding input has an invalid state -->
+    <b-form-invalid-feedback id="input-live-feedback">
+      Enter at least 3 letters
+    </b-form-invalid-feedback>
+
+    <!-- This is a form text block (formerly known as help block) -->
+    <b-form-text id="input-live-help">Your full name.</b-form-text>
+  </div>
     </form>
 
     <p>What Featues and Functions would you like to recieve up dates on?</p>
@@ -65,6 +84,17 @@ export default {
     components: {
         LazyYoutube
     },
+    computed: {
+      nameState() {
+        return this.name.length > 2 ? true : false
+      }
+    },
+    data() {
+      return {
+        name: ''
+      }
+    }
+  
 }
 </script>
 
